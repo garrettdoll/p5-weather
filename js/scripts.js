3123c47268c220ded5002e5bd48cce86af1a210a
GarrettDoll <garrettdoll@gmail.com>
2014-11-24 14:38:08 -0800

    lets go

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,38 @@
 // Docs at http://simpleweatherjs.com
 $(document).ready(function() {
+
+
+
   $.simpleWeather({
     location: 'Spokane, WA',
     woeid: '',
     unit: 'f',
+
+
+
+    //If can get the weather
     success: function(weather) {
+
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
       html += '<li class="currently">'+weather.currently+'</li>';
       html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-  
+      
+
+      //DISPLAY THE WEATHER
       $("#weather").html(html);
+
+
     },
+
+
+
     error: function(error) {
+
+
       $("#weather").html('<p>'+error+'</p>');
+
+
     }
   });
 });
