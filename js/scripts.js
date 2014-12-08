fd34d8e2c4ba93cb69dc4f02002c0fa5a2030c69
garrettdoll <Admin@Lil-KID.local>
2014-12-07 23:09:05 -0800

    New bg and search function

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,12 @@
 
 
 
-
-
-
 // Docs at http://simpleweatherjs.com
 $(document).ready(function() {
+
+
+
+
 
 
 
@@ -21,11 +22,10 @@ $(document).ready(function() {
 
     //If can get the weather
     success: function(weather) {
-
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
       html += '<li class="currently">'+weather.currently+'</li>';
       html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-      
 
 
 
@@ -55,25 +55,69 @@ $(document).ready(function() {
 
 
 
-// store names of CSS classes in an array
-var bgPat = ['bg0', 'bg1', 'bg2', 'bg3', 'bg4'];
 
-// random number function
-var getRand = function(limit) {
+
+$('.ui.button').on('click', function() {
   
-  //var limit = 3;
-  var numRand = Math.floor(Math.random()*limit);
-  console.log(numRand);
-  return numRand;
+  var zip = $('input').val();
 
-};
 
-var bgRand = getRand(5);
 
-// on page reload a number between 0-to-2
-// concatenate 'bg' plus number between 0-to-2
 
-$('body').addClass(bgPat[bgRand]);
+
+
+
+
+// Docs at http://simpleweatherjs.com
+$(document).ready(function() {
+
+
+
+
+
+
+
+  $.simpleWeather({
+    location: zip,
+    woeid: '',
+    unit: 'f',
+
+
+
+    //If can get the weather
+    success: function(weather) {
+
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+
+
+
+        for(var i=0;i<weather.forecast.length;i++) {
+        html += '<p>'+weather.forecast[i].day+': '+weather.forecast[i].high+'</p>';
+      }
+
+      //DISPLAY THE WEATHER
+      $("#weather").html(html);
+
+
+    },
+
+
+
+    error: function(error) {
+
+
+      $("#weather").html('<p>'+error+'</p>');
+
+
+    }
+  });
+});
+
+});
+
 
 
 
